127e3581bcdcee5ac3c71a40cace0dce9cfd1566
LorenzJ <lorenz_jolling@hotmail.com>
2015-04-04 21:26:53 +0200

    Add Handle and Entity Instances

--- Source/EntitySystems/include/System.h
+++ Source/EntitySystems/include/System.h
@@ -2,6 +2,7 @@
 #define ES_SYSTEM_H
 
 #include <cstddef>
+#include <cstdint>
 
 namespace es
 {
@@ -24,6 +25,33 @@ namespace es
 	public:
 		virtual void Process() = 0;
 		virtual ~System() = 0;
+
+		class Handle
+		{
+			friend class System;
+			std::int16_t _id;
+			// std::int16_t gen;
+			Handle(std::int16_t id) : _id(id) { }
+		public:
+			Handle() { }
+			std::int16_t GetId()
+			{
+				return _id;
+			}
+		};
+
+		class Reference : public Handle { };
+
+	protected:
+		static void SetHandle(Handle &h, int16_t id)
+		{
+			h._id = id;
+		}
+		
+		static Handle CreateHandle(int16_t id)
+		{
+			return Handle(id);
+		}
 	};
 }
 
